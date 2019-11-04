7d5bad108b5264bd0c7133e84a7c1f6a83a92d27
Damian Petryk <damian.petryk@infopark.de>
2019-11-04 10:35:05 +0100

    initial

---
+++ README.md
@@ -0,0 +1,43 @@
+# lh-audit
+
+Lighthouse audit (`lh-audit`) runs 5 lighthouse performance audits and displays the [geometric mean](https://en.wikipedia.org/wiki/Geometric_mean) (short "gmean") of it.
+
+![Screenshot](/screenshot.png?raw=true)
+
+It uses a headless chrome to execute lighthouse. It also uses the `devtools` throttling method.
+
+## Usage
+
+```
+lh-audit <url> [options]
+
+Options:
+  -s, --store-logs  store lighthouse logs in ./storedLogs
+  -h, --help        output usage information
+```
+
+## Metrics
+
+| Metric  | Name                          | Description               |
+| ------- | ----------------------------- | ------------------------- |
+| _p_     | The overall performance score | A value between 0 and 100 |
+| _fcp_   | First contentful paint        |                           |
+| _fmp_   | First meaningful paint        |                           |
+| _speed_ | Speed index                   |                           |
+| _tti_   | Time to interactive           |                           |
+| _cpu_   | First CPU idle                |                           |
+| _size_  | Total byte weight             |                           |
+
+## Details
+
+Each performance sub-metric (fcp, fmp, speed, tti and cpu) shows a time in seconds and in parenthesize the reached sub-score.
+
+E.g. `cpu: 7.45 s (5.1/13.3)` says, that the cpu metric took `7.45` seconds to complete and that it got a total score of `5.1` out of possibly `13.3` points. Currently the cpu metric is weighted 2 out of 15, so a 100% in cpu adds 13.3 points to the overall score.
+
+The overall performance score simply shows the achieved score.
+
+The size metric does not directly influence the overall performance. But of cause it indirectly affects the other metrics.
+
+## License
+
+MIT
---
+++ bin/lh-audit.js
@@ -0,0 +1,234 @@
+#!/usr/bin/env node
+
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const { red, yellow, green, italic, bold } = require("colors/safe");
+const path = require("path");
+const fs = require("fs");
+const assetSaver = require("lighthouse/lighthouse-core/lib/asset-saver");
+const commander = require("commander");
+const computeGmean = require("compute-gmean");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const urlsToCheck = require("./urls-to-check");
+
+const lighthouseConfig = {
+  extends: "lighthouse:default",
+  settings: {
+    onlyCategories: ["performance"],
+    throttlingMethod: "devtools",
+  },
+};
+
+const GMEAN_PREFIX = "GMEAN: ";
+
+const storedLogs = path.resolve(process.cwd(), "storedLogs");
+fs.mkdirSync(storedLogs, { recursive: true });
+
+const csvWriter = createCsvWriter({
+  path: `${storedLogs}/logged-results.csv`,
+  header: [
+    { id: "site", title: "SITE URL" },
+    { id: "type", title: "TYPE" },
+    { id: "p", title: "PERFORMANCE SCORE" },
+    { id: "fcp", title: "FIRST CONTENTFUL PAINT" },
+    { id: "fmp", title: "FIRST MEANINGFUL PAINT" },
+    { id: "speed", title: "SPEED INDEX" },
+    { id: "cpu", title: "FIRST CPU IDLE" },
+    { id: "tti", title: "TIME TO INTERACTIVE" },
+    { id: "size", title: "TOTAL BYTE WEIGHT" },
+  ],
+});
+
+
+async function runLighthouse(lighthouseUrl, storeLogs) {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const port = chrome.port;
+  const results = await lighthouse(
+    lighthouseUrl,
+    { port },
+    lighthouseConfig
+  ).catch(error => console.log(error));
+  if (results) {
+    const condensedReport = condenseReport(results.lhr);
+    addToLogs(condensedReport, lighthouseUrl, "PARTIAL");
+    console.log(
+      `${GMEAN_PREFIX.replace(/./g, " ")}${oneliner(
+        condensedReport,
+        lighthouseUrl
+      )}`
+    );
+
+    if (storeLogs) {
+      const pathWithBasename = path.resolve(
+        storedLogs,
+        `${results.artifacts.BenchmarkIndex}`
+      );
+      await assetSaver.saveAssets(
+        results.artifacts,
+        results.lhr.audits,
+        pathWithBasename
+      );
+    }
+
+    await chrome.kill();
+    return condensedReport;
+  } else {
+    logError(lighthouseUrl);
+    await chrome.kill();
+    return null;
+  }
+}
+
+// See [1] for important metrics
+// [1] https://github.com/GoogleChrome/lighthouse/blob/master/lighthouse-core/config/default-config.js#L323-L327
+function condenseReport(results) {
+  return {
+    performanceScore: results.categories.performance.score,
+    firstContentfulPaint: results.audits["first-contentful-paint"].numericValue,
+    firstContentfulPaintScore: results.audits["first-contentful-paint"].score,
+    firstMeaningfulPaint: results.audits["first-meaningful-paint"].numericValue,
+    firstMeaningfulPaintScore: results.audits["first-meaningful-paint"].score,
+    speedIndex: results.audits["speed-index"].numericValue,
+    speedIndexScore: results.audits["speed-index"].score,
+    timeToInteractive: results.audits["interactive"].numericValue,
+    timeToInteractiveScore: results.audits["interactive"].score,
+    firstCpuIdle: results.audits["first-cpu-idle"].numericValue,
+    firstCpuIdleScore: results.audits["first-cpu-idle"].score,
+    totalByteWeight: results.audits["total-byte-weight"].numericValue,
+  };
+}
+
+function oneliner(r, url) {
+  return [
+    `${italic(url)}`,
+    `${italic("p:")} ${formatScore(r.performanceScore)}`,
+    `${italic("fcp:")} ${formatAudit(r, "firstContentfulPaint", 3 / 15)}`,
+    `${italic("fmp:")} ${formatAudit(r, "firstMeaningfulPaint", 1 / 15)}`,
+    `${italic("speed:")} ${formatAudit(r, "speedIndex", 4 / 15)}`,
+    `${italic("cpu:")} ${formatAudit(r, "firstCpuIdle", 2 / 15)}`,
+    `${italic("tti:")} ${formatAudit(r, "timeToInteractive", 5 / 15)}`,
+    `${italic("size:")} ${formatBytes(r.totalByteWeight)}`,
+  ].join(", ");
+}
+
+async function addToLogs(r, url, type) {
+  await csvWriter
+    .writeRecords([
+      {
+        site: url,
+        type: type,
+        p: formatScore(r.performanceScore, false),
+        fcp: formatAudit(r, "firstContentfulPaint", 3 / 15, false),
+        fmp: formatAudit(r, "firstMeaningfulPaint", 1 / 15, false),
+        speed: formatAudit(r, "speedIndex", 4 / 15, false),
+        cpu: formatAudit(r, "firstCpuIdle", 2 / 15, false),
+        tti: formatAudit(r, "timeToInteractive", 5 / 15, false),
+        size: formatBytes(r.totalByteWeight),
+      },
+    ])
+}
+
+async function logError(url) {
+  await csvWriter
+    .writeRecords([
+      {
+        site: url,
+        type: "Error: site not loaded",
+      },
+    ])
+    .then(() => {
+      console.log("Logged error", formatScore(r.performanceScore, false));
+    });
+}
+
+function formatScore(score, colorize = true) {
+  const number = `${Math.floor(score * 100)}`;
+  if (colorize) {
+    return colorizeByScore(number.padStart(3), score);
+  }
+  return number.padStart(3);
+}
+
+function formatAudit(results, auditName, weight, colorize = true) {
+  const auditScore = results[`${auditName}Score`];
+  const text = `${formatDuration(results[auditName])} (${formatWeightedScore(
+    auditScore,
+    weight
+  )})`;
+
+  if (colorize) {
+    return colorizeByScore(text, auditScore);
+  }
+  return text;
+}
+
+function colorizeByScore(text, score) {
+  if (score > 0.89) {
+    return green(text);
+  } else if (score > 0.49) {
+    return yellow(text);
+  }
+  return red(text);
+}
+
+function formatDuration(durationInMs) {
+  const seconds = durationInMs / 1000;
+  return `${seconds.toFixed(2)} s`;
+}
+
+function formatWeightedScore(score, weight) {
+  const weightedScore = score * weight;
+  return `${formatWeight(weightedScore)}/${formatWeight(weight)}`;
+}
+
+function formatWeight(weight) {
+  return (weight * 100).toFixed(1);
+}
+
+function formatBytes(bytes) {
+  const kiloBytes = Math.round(bytes / 1000);
+  const thousand = Math.floor(kiloBytes / 1000);
+  const hundred = `${kiloBytes % 1000}`;
+  if (thousand) {
+    return `${thousand},${hundred.padStart(3, "0")} KB`;
+  }
+  return `${hundred} KB`;
+}
+
+async function multipleTimes(url, storeLogs) {
+  const multipleResults = [];
+
+  for (const j of new Array(3)) {
+    const result = await runLighthouse(url, storeLogs);
+    if (result) {
+      multipleResults.push(result);
+    }
+  }
+
+  if (multipleResults.length > 0) {
+    const gmean = {};
+    Object.keys(multipleResults[0]).forEach(metric => {
+      const valuesOfMetric = multipleResults.map(r => r[metric]);
+      gmean[metric] = computeGmean(valuesOfMetric);
+    });
+
+    console.log(`${bold(GMEAN_PREFIX)}${oneliner(gmean, url)}`);
+    addToLogs(gmean, url, "GMEAN");
+  }
+}
+
+commander
+  .description("Runs lighthouse performance tests")
+  .usage("[options]")
+  .option("-s, --store-logs", "store lighthouse logs in ./storedLogs")
+  .action(async (cmd) => {
+    if (!cmd) {
+      commander.outputHelp();
+      process.exitCode = 1;
+      return;
+    }
+    for (let i = 0; i < urlsToCheck.length; i++) {
+      await multipleTimes(urlsToCheck[i], cmd.storeLogs);
+    }
+  })
+  .parse(process.argv);
